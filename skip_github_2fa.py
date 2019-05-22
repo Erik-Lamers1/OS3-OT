19364d1af2f70995778ad38fb7b91a5eee997f6a
Erik Lamers <erik@Eriks-MacBook-Air.local>
2019-05-22 20:46:57 +0200

    Try auth post

--- skip_github_2fa.py
+++ skip_github_2fa.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from seleniumrequests import Firefox
 from argparse import ArgumentParser
 
 
+DOMAIN = 'https://serverdesity.io'
+
 def parse_args(args=None):
     parser = ArgumentParser(description='Script for OT demo qith searchable 2FA codes. '
-                                        'Currently configured to work on serverdensity.io')
+                                        'Currently configured to work on {}'.format(DOMAIN))
     parser.add_argument('-u', '--username', required=True, help='The username')
     parser.add_argument('-p', '--password', required=True, help='The password')
     return parser.parse_args()
@@ -15,7 +17,14 @@ def parse_args(args=None):
 def main(args=None):
     args = parse_args(args)
     webdriver = Firefox()
-    webdriver.request('GET', 'https://serverdensity.io')
+    webdriver.get('https://serverdensity.io')
+    response = webdriver.request('POST', '{}/sessions'.format(DOMAIN), data={'username': args.username,
+                                                 'password': args.password,
+                                                 'MFAotpcode': 'blaap',
+                                                 'MFAremember': 'false',
+                                                 'accountName': args.username}
+                      )
+    print(response)
 
 
 if __name__ == '__main__':
